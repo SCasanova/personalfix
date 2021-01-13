55267a7ec76deabb33ae505ebfe198484b83f546
Santiago Casanova <your_email@example.com>
2021-01-12 18:42:06 -0600

    Added advanced passing functions (v0.2.0)

--- R/pull_fun.R
+++ R/pull_fun.R
@@ -116,3 +116,121 @@ clean_name <- function(to_remove, data_clean){
   }
   return(data_clean)
 }
+
+#' Pull NFL Advanced Team Passing Stats
+#'
+#' This function takes an argument for desired years and outputs a data.table
+#' containing all Pro Football Reference team advanced passing stats from selected years.
+#'
+#'
+#'
+#' @param seasons Range of years to pull from pfref. Defaults to 2020
+#' @return A data table including team advancedpasssing stats from selected
+#' years
+#' @importFrom magrittr "%>%"
+#' @import data.table
+#' @export
+
+pull_teamAdvPass <- function(seasons=2020){
+  data_name <- data.table::data.table()
+  for (x in seasons){
+    adv <- (xml2::read_html(url(paste0("https://www.pro-football-reference.com/years/",x,"/advanced.htm"))) %>% #read and interpret html table
+              rvest::html_table(fill = T))[[1]]
+    colnames(adv) <- adv[1,]
+    delete <- rownames(adv[adv$Tm == 'Tm',])
+    for (i in 2:3){
+      tab <- (xml2::read_html(url(paste0("https://www.pro-football-reference.com/years/",x,"/advanced.htm"))) %>% #read and interpret html table
+                rvest::html_table(fill = T))[[i]]
+      colnames(tab) <- tab[1,]
+      delete <- rownames(tab[tab$Tm == 'Tm',]) %>% #delete intermediate column-name rows
+        as.numeric()
+      tab <- tab[-delete, c(1, 6:ncol(tab))]
+      adv <- merge(adv, tab, by = 'Tm')
+    }
+    adv <- data.table::data.table(adv)
+    adv[, Season := x]
+    data_name <- rbind(data_name, adv)
+  }
+
+  #Clean data
+  data_name$`Drop%` <- gsub("%", "", data_name$`Drop%`, fixed = T)
+  data_name$`Bad%` <- gsub("%", "", data_name$`Bad%`, fixed = T)
+  data_name$`OnTgt%` <- gsub("%", "", data_name$`OnTgt%`, fixed = T)
+  data_name$`Prss%` <- gsub("%", "", data_name$`Prss%`, fixed = T)
+
+  data_name <- data_name %>% dplyr::mutate_at(c(2:30), as.numeric)
+  data_name$`Drop%` <- data_name$`Drop%`/100
+  data_name$`Bad%` <- data_name$`Bad%`/100
+  data_name$`OnTgt%` <- data_name$`OnTgt%`/100
+  data_name$`Prss%` <- data_name$`Prss%`/100
+  data_name[is.na(data_name)] <- 0
+
+  return(data_name)
+}
+
+#' Pull NFL Advanced Passing Stats
+#'
+#' This function takes an argument for desired years and outputs a data.table
+#' containing all Pro Football Reference individual advanced passing stats from selected years.
+#'
+#'
+#'
+#' @param seasons Range of years to pull from pfref. Defaults to 2020
+#' @return A data table including individual advanced passsing stats from
+#' selected years
+#' @importFrom magrittr "%>%"
+#' @import data.table
+#' @export
+
+pull_AdvPass <- function(seasons=2020){
+  try(if(min(seasons) < 2018) stop('No advanced data available for seasons before 2018', call. = F))
+  data_name <- data.table::data.table()
+  for (x in seasons){
+    adv <- (xml2::read_html(url(paste0("https://www.pro-football-reference.com/years/",x,"/passing_advanced.htm"))) %>% #read and interpret html table
+              rvest::html_table(fill = T))[[1]]
+    colnames(adv) <- adv[1,]
+    delete <- rownames(adv[adv$Tm == 'Tm',]) %>% as.numeric()
+    adv <- adv[-(delete), ]
+    for (i in 2:3){
+      tab <- (xml2::read_html(url(paste0("https://www.pro-football-reference.com/years/",x,"/passing_advanced.htm"))) %>% #read and interpret html table
+                rvest::html_table(fill = T))[[i]]
+      colnames(tab) <- tab[1,]
+      delete <- rownames(tab[tab$Tm == 'Tm',]) %>% #delete intermediate column-name rows
+        as.numeric()
+      tab <- tab[-delete, c(2, 10:ncol(tab))]
+      adv <- merge(adv, tab, by = 'Player')
+    }
+    adv <- data.table::data.table(adv)
+    adv <- adv %>% select(-c(Yds,Yds.y))
+    adv <- adv %>% rename(Yds = Yds.x)
+    adv[, Season := x]
+    adv[,ProBowl := ifelse(Att >=50, ifelse(grepl("*", y$Player, fixed = T), 1, 0), 0)]
+    adv[,AllPro :=  ifelse(Att <= 50, ifelse(grepl("+", y$Player, fixed = T), 1, 0), 0)]
+    data_name <- rbind(data_name, adv)
+  }
+
+  #Clean data
+  remove_vec <- c("*", "+", ".", "Jr", "Sr", "III", "II")
+  data_name$Player <- clean_name(remove_vec, data_name$Player)
+  data_name$`Drop%` <- gsub("%", "", data_name$`Drop%`, fixed = T)
+  data_name$`Bad%` <- gsub("%", "", data_name$`Bad%`, fixed = T)
+  data_name$`OnTgt%` <- gsub("%", "", data_name$`OnTgt%`, fixed = T)
+  data_name$`Prss%` <- gsub("%", "", data_name$`Prss%`, fixed = T)
+
+  data_name <- data_name %>% dplyr::mutate_at(c(6:38), as.numeric)
+  data_name$`Drop%` <- data_name$`Drop%`/100
+  data_name$`Bad%` <- data_name$`Bad%`/100
+  data_name$`OnTgt%` <- data_name$`OnTgt%`/100
+  data_name$`Prss%` <- data_name$`Prss%`/100
+  data_name[is.na(data_name)] <- 0
+
+  return(data_name)
+}
+
+clean_name <- function(to_remove, data_clean){
+  for (i in 1:length(to_remove)){
+    data_clean <- gsub(paste0(to_remove[i]), "", data_clean, fixed = T)
+    data_clean <- trimws(data_clean)
+  }
+  return(data_clean)
+}
