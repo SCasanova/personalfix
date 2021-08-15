e654e66134e1fa70c7f08b8f5827dddd0caeb62f
Santiago Casanova <your_email@example.com>
2021-08-15 14:49:52 -0500

    Replaced base iwht purrr/stringr

--- R/nfl_funs.R
+++ R/nfl_funs.R
@@ -19,9 +19,7 @@ clean_roster <- function(seasons = 2020) {
       position = dplyr::case_when(position == 'FB' |
                                     position == 'HB' ~ 'RB',
                                   T ~ position),
-      headshot_url = sapply(headshot_url, function(x) {
-        gsub('http:', 'https:', x, fixed = T)
-      })
+      headshot_url = stringr::str_replace(headshot_url, 'http:', 'https:')
     ) %>% dplyr::left_join(
       ffscrapr::dp_playerids() %>% dplyr::select(gsis_id, draft_year, draft_round, draft_pick) %>% dplyr::filter(!is.na(gsis_id)),
       by = 'gsis_id',
@@ -43,11 +41,15 @@ clean_roster <- function(seasons = 2020) {
 #' @importFrom magrittr "%>%"
 #' @export
 
-name_key <- function(name, draft_year, draft_round, position){
+name_key <- function(name, draft_year, draft_round, position) {
   key <- ffscrapr::dp_clean_names(name, lowercase = T)
   paste0(
-    purrr::map_chr(key, function(x){stringr::str_sub(stringr::str_split(x, ' ')[[1]][1], 1,3)}),
-    purrr::map_chr(key, function(x){stringr::str_split(x, ' ')[[1]][2]}),
+    purrr::map_chr(key, function(x) {
+      stringr::str_sub(stringr::str_split(x, ' ')[[1]][1], 1, 3)
+    }),
+    purrr::map_chr(key, function(x) {
+      stringr::str_split(x, ' ')[[1]][2]
+    }),
     stringr::str_sub(draft_year, 3, 4),
     draft_round,
     position
@@ -71,37 +73,50 @@ name_key <- function(name, draft_year, draft_round, position){
 #' @importFrom magrittr "%>%"
 #' @export
 
-foutsiders_data <- function(page = 'team-offense', season = 2020, user='', pass='') {
-    if( user=='' | pass =='' ) {warning('No user or pass detected, data may be incomplate')}
+foutsiders_data <-
+  function(page = 'team-offense',
+           season = 2020,
+           user = '',
+           pass = '') {
+    if (user == '' |
+        pass == '') {
+      warning('No user or pass detected, data may be incomplate')
+    }
 
-  login <-
-    paste0('https://www.footballoutsiders.com/stats/nfl/',page,'/',season)
+    login <-
+      paste0('https://www.footballoutsiders.com/stats/nfl/',
+             page,
+             '/',
+             season)
 
-  pgsession <- rvest::session(login)
-  pgform <- rvest::html_form(pgsession)[[3]]
-  filled_form <-
-    rvest::html_form_set(pgform, name = user, pass = pass)
-  rvest::session_submit(pgsession, filled_form)
+    pgsession <- rvest::session(login)
+    pgform <- rvest::html_form(pgsession)[[3]]
+    filled_form <-
+      rvest::html_form_set(pgform, name = user, pass = pass)
+    rvest::session_submit(pgsession, filled_form)
 
-  page_read <-
-    rvest::session_jump_to(pgsession,
-                    login)
-  table <- (page_read %>% rvest::html_table(fill = T))[[1]]
-  if( is.na(table[6,2]) | table[6,2] == '' ) {warning('Login failed, check your credentials. Data may be incomplate')}
-  if(stringr::str_detect(page, 'line')){
-    colnames(table) <- table[1,]
-    table <- table[-1,]
+    page_read <-
+      rvest::session_jump_to(pgsession,
+                             login)
+    table <- (page_read %>% rvest::html_table(fill = T))[[1]]
+    if (is.na(table[6, 2]) |
+        table[6, 2] == '') {
+      warning('Login failed, check your credentials. Data may be incomplate')
+    }
+    if (stringr::str_detect(page, 'line')) {
+      colnames(table) <- table[1, ]
+      table <- table[-1, ]
+    }
+
+    options(warn = -1)
+    table <- table %>%
+      dplyr::select(which(sapply(., is.character))) %>%
+      dplyr::mutate(dplyr::across(dplyr::everything(), fix_num))
+    options(warn = 1)
+
+    table
+
   }
-
-  options(warn =-1)
-  table <- table %>%
-    dplyr::select(which(sapply(.,is.character))) %>%
-    dplyr::mutate(dplyr::across(dplyr::everything(), fix_num))
-  options(warn=1)
-
-  table
-
-}
 
 #' Fix number format in vector
 #'
@@ -121,7 +136,7 @@ foutsiders_data <- function(page = 'team-offense', season = 2020, user='', pass=
 fix_num <- function(nums) {
   if (stringr::str_detect(nums[1], "%$")) {
     nums <- nums %>%
-      stringr::str_remove(., "%" ) %>%
+      stringr::str_remove(., "%") %>%
       as.numeric()
     nums / 100
   } else if (stringr::str_detect(nums[1], "[0-9]\\.?,?[0-9]*$")) {
@@ -132,6 +147,3 @@ fix_num <- function(nums) {
     nums
   }
 }
-
-
-
